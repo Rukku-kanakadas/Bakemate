bdcd51f821d0a6601dd7ce1b97cc66e4e34eac05
Rukku Kanakadas <167336833+Rukku-kanakadas@users.noreply.github.com>
2025-07-28 23:04:19 +0530

    Update vite.config.ts
    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Bakemate/',  // 👈 Add this line to set your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
